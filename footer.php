49b247ec6f4d2924f576de5bd1ad7f98af860b15
Stéphane Leblond <leblond.stephane@gmail.com>
2013-07-04 11:34:21 +0200

    Ajout d'un lien vers la licence GPL

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <img src="images/tux_ping.png" alt="tux_ping" align="center" width=100 height=100 /><br><br>
 <a href="index.php">Retour page accueil</a>
 <h4>
-<p>Script PHP réalisé par Stéphane Leblond sous licence GPL<br>
+<p>Script PHP réalisé par Stéphane Leblond sous <a href = "http://www.gnu.org/licenses/gpl-3.0.fr.html"target="_blank">licence GPL</a><br>
 </p>
 </h4> 
 <? include("compteur.php"); ?>
