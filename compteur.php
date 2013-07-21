74a21301f7f7244cdc31a1731b43d690084dadf1
Stéphane Leblond <leblond.stephane@gmail.com>
2013-07-21 12:43:57 +0200

    Ajout d'une fonction de création du fichier compteur.txt s'il n'existe pas

--- compteur.php
+++ compteur.php
@@ -1,5 +1,6 @@
 <?php
 //header('content-type:text/html; charset=utf-8');
+if (file_exists("compteur.txt")) {
 $monfichier = fopen('compteur.txt', 'r+');
 $pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
 $pages_vues++; // On augmente de 1 ce nombre de pages vues
@@ -7,4 +8,14 @@ fseek($monfichier, 0); // On remet le curseur au début du fichier
 fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
 fclose($monfichier);
 echo '<p>Il y a eu ' . $pages_vues . ' pages visitées !</p>';
+} else {
+$pages_vues = 1;
+touch ("compteur.txt" , 0777);
+$monfichier = fopen('compteur.txt', 'r+');
+fseek($monfichier, 0); 
+fputs($monfichier, $pages_vues);
+fclose($monfichier);
+echo '<p>Il y a eu ' . $pages_vues . ' pages visitées !</p>';
+}
 ?>
+
